28179ea22946dc2e0e467bc33a3f1d2160fb41fc
aeonshad <shadaeon@gmail.com>
2024-09-26 13:05:37 +0200

    Add Header and Footer

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,30 @@
+function Footer() {
+    return (
+        <footer className="w-full border-t border-border">
+            <div className="m-auto w-full max-w-3xl px-2 py-4">
+                <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between">
+                    <div className="flex flex-row items-center gap-2">
+                        <img src="./img/Logo.png" alt="HRnet Logo" width={40} height={30} />
+                        <a href="/">HRnet</a>
+                    </div>
+                    <div className="flex flex-col items-end gap-2 text-sm text-muted-foreground">
+                        <a className="hover:underline" href="/">
+                            Privacy
+                        </a>
+                        <a className="hover:underline" href="/">
+                            CGV
+                        </a>
+                        <a className="hover:underline" href="/">
+                            Employees
+                        </a>
+                    </div>
+                </div>
+                <div className="flex w-full items-center justify-center">
+                    <p className="text-xs text-muted-foreground">&copy; {new Date().getFullYear()} Wealth Health - HRnet</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+    return (
+        <header className="sticky top-0 z-40 w-full border-b bg-background">
+            <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+                <div className="flex items-center gap-2">
+                    <img src="./img/Logo.png" alt="HRnet Logo" width={50} height={35} />
+                    <div className="flex items-baseline gap-2">
+                        <a className="scroll-m-20 text-xl font-semibold tracking-tight font-caption" href="/">
+                            HRnet
+                        </a>
+                        <a className="font-medium hover:underline text-muted-foreground hover:text-foreground" href="/">
+                            Employees
+                        </a>
+                    </div>
+                </div>
+                <div className="flex flex-1 items-center justify-end space-x-4">
+                    <nav className="flex items-center space-x-1">
+                        <p>Toggle theme </p>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default Header;
